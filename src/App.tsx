321bae27fdf61351a2a94f7edd427fa9078bc5fc
Kacper <kacpermaciejewski1998@gmail.com>
2023-10-05 19:18:34 +0200

    react-router-dom init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Outlet, Route } from "react-router-dom";
+
 
 
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route index path="/" element={<Root />}></Route>
+  ))
   return (
-<h1 className="font-bold">Hello world</h1>
+<RouterProvider router={router}></RouterProvider>
   );
 }
+
+
+const Root = () => {
+  return (
+    <>
+    <Outlet />
+    </>
+  )
+}
+
 
 export default App;
