32d259803da5f8522d15cf4b22be98141aecb838
Kacper <kacpermaciejewski1998@gmail.com>
2023-10-27 17:04:14 +0200

    connecting hero with api

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,43 +3,27 @@ import hero from "../assets/hero.jpg"
 import hero2 from "../assets/hero2.jpg"
 import hero3 from "../assets/hero3.webp"
 import HeroItem from './HeroItem'
+import useFetch from '../hooks/useFetch'
+import Loading from './Loading'
 
 
-type Feed = {
-    id: number,
-    img: string,
-    title: string
-}
+
 
 const Hero = () => {
+const {data, isLoading, error} = useFetch("posts")
+const trimmedData = data.slice(0,3)
 
-
-    const feed: Feed[] = [
-        {   
-            id: 1,
-            img: hero,
-            title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, laudantium.1"
-        },
-        {   
-            id: 2,
-            img: hero2,
-            title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, laudantium.2"
-        },
-        {
-            id: 3,
-            img: hero3,
-            title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, laudantium.3"
-        },
-    ]
-
+if(isLoading){
+    return  <Loading />
+  }
     
   return (
     <section className='mt-10'>
     <h2 className='px-2 text-2xl mb-4 uppercase text-blue-700 font-bold'>Najnowsze wpisy</h2>
     <div className='flex flex-col gap-7 lg:grid lg:grid-cols-2 lg:gap-0'>
-    {
-            feed.map((item:Feed) => <HeroItem key={item.id}  img={item.img} title={item.title}/>)
-        }
+    { 
+     data && trimmedData.map((item: any) => <HeroItem key={item.id} id={item.id} img={item.attributes.img.data?.attributes.url} title={item.attributes.title}/>)
+         }
     </div>
         
     </section>
--- src/components/HeroItem.tsx
+++ src/components/HeroItem.tsx
@@ -2,23 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 type HeroItemProps = {
+    id: number,
     img: string,
     title: string
 }
 
 
-const HeroItem = ({img, title}:HeroItemProps) => {
+const HeroItem = ({id, img, title}:HeroItemProps) => {
     console.log(img)
   return (
     <article style={{
-        backgroundImage: `url(${img})`
+        backgroundImage: `url(http://localhost:1337${img})`
     }}
     className='h-52 bg-cover bg-center relative lg:first-of-type:h-full lg:first-of-type:row-start-1 lg:first-of-type:row-end-4 lg:first-of-type:text-2xl'
     >
         <div
         className='bg-opacity-70 bg-black h-1/3 absolute bottom-0 left-0 right-0 px-4 flex items-center text-white hover:underline'
         >
-            <Link to={`/article/${title}`}>{title}</Link>
+            <Link to={`/post/${id}`}>{title}</Link>
         </div>
 
     </article>
